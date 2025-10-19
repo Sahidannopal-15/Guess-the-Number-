7f82748f3fe18f6bc24d4f74121f42e9c107cfed
Sahidannopal-15 <muhammadnaufalsahidan123@gmail.com>
2025-10-19 22:26:14 +0700

    menambahkan fitur reset, hint, history.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,52 @@
-import React from 'react'
+import React, { use } from 'react'
 import { useState } from "react";
 
 function App(){
   const [randomNumber, setRandomNumber] = useState(Math.floor(Math.random() *100) + 1) ;
   const [guess, setGuess] = useState('');
   const [result, setResult] = useState('');
-
-function tebakAngka(){
+  const [hint, setHint] = useState('');
+  const [history, setHistory] = useState([]);
+  
+  function tebakAngka(){
+    setHistory([...history, guess]);
+    const hitungSelisih = Math.abs(guess - randomNumber);
   if(guess < randomNumber){
     setResult("Terlalu Kecil, Pak!");
   }else if(guess > randomNumber){
     setResult("Kegedean, Pak!");
   }else{
     setResult("Bener, King!");
+    setHint('');
+    return;
+  }
+  if(hitungSelisih <= 10){
+     setHint ("Dikit lagi, Pak!");
+  }else if(hitungSelisih <= 20){
+    setHint ("Yok bisa yok!");
+  }else{
+    setHint ("Gilaa masih jauhhh!");
   }
 } 
+
+function reset(){
+  setRandomNumber(Math.floor(Math.random()*100)+1);
+  setGuess('');
+  setResult('');
+  setHistory([]);
+}
   return (
     <main className='flex flex-col items-center min-h-screen bg-black text-white gap-8 pt-8'>
       <header className='text-2xl font-bold'>
         Guessing the Number
       </header>
-       <div className="grid grid-cols-1 lg:grid-cols-1 gap-8 max-w-7xl mx-auto items-center ">
+       <div className="grid grid-cols-1 lg:grid-cols-1 gap-8 max-w-md mx-auto items-center ">
         <div className="bg-white p-6 rounded-xl shadow-lg border border-gray-200 lg:col-span-1">
           <h3 className="text-xl font-bold mb-3 text-indigo-700 border-b pb-2">Informasi Mekanisme</h3>
           <p className="text-base text-gray-700">
 ⚙️ Mekanisme Permainan Utama
-Permainan Tebak Angka memiliki alur yang sangat mudah dipahami, menjadikannya ideal untuk segala usia dan platform : <br />
-Penentuan Angka Rahasia: Komputer (atau sistem) secara acak memilih satu angka bulat dalam rentang yang telah ditentukan antara 1 dan 100. Angka ini disembunyikan dari pemain. <br />
+Permainan Tebak Angka : <br />
+Penentuan Angka Rahasia: Komputer secara acak memilih satu angka bulat dalam rentang yang telah ditentukan antara 1 dan 100. Angka ini disembunyikan dari pemain. <br />
 Input Pemain: Pemain memasukkan tebakan mereka. <br />
 Umpan Balik : Setelah pemain memasukkan angka, sistem akan memberikan petunjuk apakah angka tebakan mereka terlalu Tinggi atau terlalu Rendah dari angka rahasia.
     </p>
@@ -44,10 +64,27 @@ Umpan Balik : Setelah pemain memasukkan angka, sistem akan memberikan petunjuk a
         className='bg-purple-500 px-4 py-2 rounded-2xl hover:bg-purple-400'>
           submit
         </button>
+        <button 
+        onClick={reset}
+        className='mt-3 bg-purple-500 px-4 py-2 rounded-2xl hover:bg-purple-400'
+        >
+          Reset
+        </button>
       </section>
       <article>
         <p>{result}</p>
+        <p>{hint}</p>
+        <ul>
+          {history.map((item, index) => (
+          <li key={index}>
+            tebakan ke {index + 1} : {item};
+          </li>
+          ))}
+        </ul>
       </article>
+      <footer>
+        
+      </footer>
     </main>
   )
 }
