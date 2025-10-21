cb6022ad66ba117b9d909db110cdc56263e681bb
Sahidannopal-15 <muhammadnaufalsahidan123@gmail.com>
2025-10-21 20:10:21 +0700

    Update : Mobile friendly

--- src/App.jsx
+++ src/App.jsx
@@ -37,14 +37,14 @@ function reset(){
   setHistory([]);
 }
   return (
-    <main className='flex h-screen bg-gradient-to-r from-black via-gray-900 to-black text-white'>
+    <main className='flex flex-col md:flex-row h-screen md:bg-gradient-to-r from-black via-gray-900 to-black text-white'>
       <section className='flex-1 flex flex-col items-center mt-8 gap-4'>
-        <p className='text-2xl font-extrabold tracking-wide'>🎯Tebak Angka 1-100</p>
+        <p className='text-xl md:text-2xl font-extrabold tracking-wide'>🎯 Tebak Angka 1-100</p>
         <input type="number" 
                 placeholder='Masukan Angka'
                 value={guess}
                 onChange={(e) => setGuess(e.target.value)}
-                className='mb-3 p-2 rounded-2xl bg-cyan-50 text-black hover:bg-gray-300 font-semibold'
+                className='text-sm md:text-base mb-3 p-2 rounded-2xl bg-cyan-50 text-black hover:bg-gray-300 font-semibold'
         />
         <button 
         onClick={tebakAngka}
@@ -57,13 +57,14 @@ function reset(){
         >
           Reset
         </button>
-          <p className='font-bold tracking-wide text-xl text-green-400 drop-shadow-[0_0_10px_rgba(34,197,94,0.8)]'>{result}</p>
-          <footer className='mt-28 border-t border-white/10 text-sm italic hover:text-red-500 transition-colors duration-300 tracking-widest'>
+          <p className='font-bold tracking-wide text-lg md:text-xl text-green-400 drop-shadow-[0_0_10px_rgba(34,197,94,0.8)]'>
+            {result}</p>
+          <footer className='mt-24 md:mt-28 py-3 border-t border-white/10 text-sm italic hover:text-red-500 transition-colors duration-300 tracking-widest'>
             💻 dibuat oleh sahidannopal15
           </footer>
       </section>  
-      <div className='flex flex-col bg-gradient-to-b from-red-900 to-rose-800 w-[43%] h-full overflow-y-auto'>
-          <p className='text-lg font-semibold mt-8 text-center'>👀 Petunjuk: {hint}</p>
+      <div className='flex flex-col bg-gradient-to-b from-red-900 to-rose-800 w-full md:w-[43%] h-full overflow-y-auto'>
+          <p className='text-xl md:text-2xl font-semibold mt-8 text-center'>👀 Petunjuk: {hint}</p>
         <ul>
           {history.map((item, index) => (
           <li className='font-mono pt-8 flex justify-center items-center border-b border-white/10' key={index}>
