e1745863debfe0a18be2071609b7fcf0ce25f286
Sahidannopal-15 <muhammadnaufalsahidan123@gmail.com>
2025-10-21 17:09:06 +0700

    updating ui

--- src/App.jsx
+++ src/App.jsx
@@ -11,34 +11,35 @@ function App(){
   function tebakAngka(){
     setHistory([...history, guess]);
     const hitungSelisih = Math.abs(guess - randomNumber);
-  if(guess < randomNumber){
-    setResult("Terlalu Kecil, Pak!");
-  }else if(guess > randomNumber){
-    setResult("Kegedean, Pak!");
-  }else{
-    setResult("Bener, King!");
-    setHint('');
-    return;
-  }
-  if(hitungSelisih <= 10){
-     setHint ("Dikit lagi, Pak!");
-  }else if(hitungSelisih <= 20){
-    setHint ("Yok bisa yok!");
-  }else{
-    setHint ("Gilaa masih jauhhh!");
-  }
+    if(guess < randomNumber){
+      setResult("Terlalu Kecil, Pak!");
+    }else if(guess > randomNumber){
+      setResult("Kegedean, Pak!");
+    }else{
+      setResult("Bener, King!");
+      setHint('');
+      return;
+    }
+    if(hitungSelisih <= 10){
+      setHint ("Dikit lagi, Pak!");
+    }else if(hitungSelisih <= 20){
+      setHint ("Yok bisa yok!");
+    }else{
+      setHint ("Gilaa masih jauhhh!");
+    }
 } 
 
 function reset(){
   setRandomNumber(Math.floor(Math.random()*100)+1);
   setGuess('');
   setResult('');
+  setHint('');
   setHistory([]);
 }
   return (
-    <main className='flex h-screen bg-black text-white'>
+    <main className='flex h-screen bg-gradient-to-r from-black via-gray-900 to-black text-white'>
       <section className='flex-1 flex flex-col items-center mt-8 gap-4'>
-        <p className='text-2xl font-bold'>🎯 Tebak Angka 1-100</p>
+        <p className='text-2xl font-extrabold tracking-wide'>🎯Tebak Angka 1-100</p>
         <input type="number" 
                 placeholder='Masukan Angka'
                 value={guess}
@@ -47,26 +48,26 @@ function reset(){
         />
         <button 
         onClick={tebakAngka}
-        className='bg-purple-500 px-4 py-2 rounded-2xl hover:bg-purple-400'>
+        className='bg-purple-500 px-4 py-2 rounded-2xl hover:bg-purple-400 transition-transform duration-200 hover:scale-105 active:scale-95 shadow-md shadow-purple-700/40'>
           Tebak
         </button>
         <button 
         onClick={reset}
-        className=' bg-purple-500 px-4 py-2 rounded-2xl hover:bg-purple-400'
+        className='bg-purple-500 px-4 py-2 rounded-2xl hover:bg-purple-400 transition-transform duration-200 hover:scale-105 active:scale-95 shadow-md shadow-purple-700/40'
         >
           Reset
         </button>
-          <p>{result}</p>
-          <footer className='mt-28'>
-            Contributor
+          <p className='font-bold tracking-wide text-xl text-green-400 drop-shadow-[0_0_10px_rgba(34,197,94,0.8)]'>{result}</p>
+          <footer className='mt-28 border-t border-white/10 text-sm italic hover:text-red-500 transition-colors duration-300 tracking-widest'>
+            💻 dibuat oleh sahidannopal15
           </footer>
       </section>  
-      <div className='flex flex-col bg-green-900 w-[43%] h-full overflow-y-auto'>
-          <p className='font-xl font-semibold mt-8 text-center'>Petunjuk: {hint}</p>
+      <div className='flex flex-col bg-gradient-to-b from-red-900 to-rose-800 w-[43%] h-full overflow-y-auto'>
+          <p className='text-lg font-semibold mt-8 text-center'>👀 Petunjuk: {hint}</p>
         <ul>
           {history.map((item, index) => (
-          <li className='pt-8 flex justify-center items-center' key={index}>
-            tebakan ke {index + 1} : {item}
+          <li className='font-mono pt-8 flex justify-center items-center border-b border-white/10' key={index}>
+            ⌛ tebakan ke {index + 1} : {item}
           </li>
           ))}
         </ul>
